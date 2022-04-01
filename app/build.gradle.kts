ed86ea1cd8135313426d10306a40be5dd4c3d620
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-01 17:40:21 +0430

    Update androidx.appcompat library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
