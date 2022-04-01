9608f174a00312657c9a9fb73de8098c83d85e71
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-01 17:47:02 +0430

    Update androidx.activity-compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.4.0")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
