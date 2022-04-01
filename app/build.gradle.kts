00499eaf0275e7e4c45cca85bb4373540318ff48
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-01 17:45:12 +0430

    Update androidx.lifecycle library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.activity:activity-compose:1.3.0-alpha06")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
