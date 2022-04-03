ff8ed98c4e7e9622f9c5bcbd592e8daedee6a61d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-03 21:05:03 +0430

    Update the layout of the "AddUserActivity"

--- app/src/main/kotlin/ir/mahozad/workout_logger/AddUserActivity.kt
+++ app/src/main/kotlin/ir/mahozad/workout_logger/AddUserActivity.kt
@@ -3,17 +3,19 @@ package ir.mahozad.workout_logger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import ir.mahozad.workout_logger.ui.theme.WorkoutLoggerTheme
 
 class AddUserActivity : ComponentActivity() {
@@ -35,15 +37,19 @@ fun AddUserScreen() {
     val focusRequester = FocusRequester()
     Column {
         Text(stringResource(R.string.user_information))
-        Text(stringResource(R.string.user_name))
-        TextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = { Text(stringResource(R.string.fist_name_label)) },
-            modifier = Modifier
-                .focusRequester(focusRequester)
-                .testTag("input-first-name")
-        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            Text(stringResource(R.string.user_name))
+            Spacer(modifier = Modifier.width(10.dp))
+            TextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = { Text(stringResource(R.string.fist_name_label)) },
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .testTag("input-first-name")
+            )
+        }
     }
     DisposableEffect(Unit) {
         focusRequester.requestFocus()
