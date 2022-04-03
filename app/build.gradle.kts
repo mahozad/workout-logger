db755e04b5cbabd753a74b39d6b048f9f23df346
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-03 18:46:12 +0430

    Add a library dependency for testing intents

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
