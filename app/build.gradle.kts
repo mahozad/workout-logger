0ba96821fe12c43da25249344d66f30f62271699
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-06 20:45:26 +0430

    Add AssertJ library for instrumented tests (androidTest)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
     testImplementation("androidx.room:room-testing:$roomVersion")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.1.3")
     androidTestImplementation("io.mockk:mockk-android:1.12.3")
+    androidTestImplementation("org.assertj:assertj-core:3.22.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
