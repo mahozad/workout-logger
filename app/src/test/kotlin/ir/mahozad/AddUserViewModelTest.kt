e7be6ff8363fcb3557a3bf60d6a8107a8258f85a
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-06 19:55:35 +0430

    Update the viewModel test to use mocked version of repository

--- app/src/test/kotlin/ir/mahozad/AddUserViewModelTest.kt
+++ app/src/test/kotlin/ir/mahozad/AddUserViewModelTest.kt
@@ -1,24 +1,35 @@
 package ir.mahozad
 
 import ir.mahozad.workout_logger.UserRepository
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn as returns
+import org.mockito.kotlin.whenever as every
 
+@ExtendWith(MockitoExtension::class)
 class AddUserViewModelTest {
 
+    @Mock lateinit var userRepository: UserRepository
+
     @Test fun `Initially, getAllUsers should be empty`(): Unit = runBlocking {
-        val userRepository = UserRepository()
+        every(userRepository.getAllUsers()) returns emptyFlow()
         val viewModel = AddUserViewModel(userRepository)
         val users = viewModel.getAllUsers()
         assertThat(users.toList()).isEmpty()
     }
 
     @Test fun `Adding a new user should succeed`(): Unit = runBlocking {
-        val userRepository = UserRepository()
-        val viewModel = AddUserViewModel(userRepository)
+        val repositoryUser = User("John", "Smith", "Man", "24")
+        every(userRepository.getAllUsers()) returns flowOf(repositoryUser)
         val user = User("John", "Smith", "Man", "24")
+        val viewModel = AddUserViewModel(userRepository)
         val wasSuccessful = viewModel.addUser(user)
         val users = viewModel.getAllUsers()
         assertThat(wasSuccessful).isTrue()
