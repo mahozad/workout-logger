fac548d627bf1b81e339ba9999a6165d4c5356c4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-08 23:11:23 +0430

    Add workout image in the WorkoutActivity

---
+++ app/src/androidTest/kotlin/ir/mahozad/workout_logger/WorkoutActivityTest.kt
@@ -0,0 +1,33 @@
+package ir.mahozad.workout_logger
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ir.mahozad.workout_logger.ui.theme.WorkoutLoggerTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WorkoutActivityTest {
+
+    @get:Rule val composeTestRule = createAndroidComposeRule<UsersActivity>()
+
+    @Test fun theWorkoutImageShouldBeDisplayed() {
+        composeTestRule.setContent {
+            WorkoutLoggerTheme {
+                WorkoutScreen()
+            }
+        }
+        composeTestRule.onNodeWithTag("image").assertIsDisplayed()
+    }
+
+    @Test fun theWorkoutImageAttributionShouldBeDisplayed() {
+        composeTestRule.setContent {
+            WorkoutLoggerTheme {
+                WorkoutScreen()
+            }
+        }
+        composeTestRule.onNodeWithTag("image-attribution").assertIsDisplayed()
+    }
+}
--- app/src/main/kotlin/ir/mahozad/workout_logger/WorkoutActivity.kt
+++ app/src/main/kotlin/ir/mahozad/workout_logger/WorkoutActivity.kt
@@ -1,9 +1,70 @@
 package ir.mahozad.workout_logger
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import dagger.hilt.android.AndroidEntryPoint
+import ir.mahozad.workout_logger.ui.theme.WorkoutLoggerTheme
 
 @AndroidEntryPoint
 class WorkoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkoutLoggerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    WorkoutScreen()
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun WorkoutScreen() {
+    val uriHandler = LocalUriHandler.current
+    Column {
+        Image(
+            painterResource(R.drawable.pushup),
+            contentDescription = stringResource(R.string.workout_image_description),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().testTag("image")
+        )
+        val annotatedString = buildAnnotatedString {
+            append("Workout vectors by ")
+            pushStringAnnotation(
+                tag = "link",
+                annotation = "https://www.vecteezy.com/free-vector/workout"
+            )
+            withStyle(style = SpanStyle(color = MaterialTheme.colors.primary)) {
+                append("Vecteezy")
+            }
+            pop()
+        }
+        ClickableText(
+            annotatedString,
+            modifier = Modifier.testTag("image-attribution"),
+            onClick = { offset ->
+                annotatedString
+                    .getStringAnnotations(tag = "link", start = offset, end = offset)
+                    .firstOrNull()
+                    ?.let { uriHandler.openUri(it.item) }
+            })
+    }
 }
