faf34122b131561afacc0f1b7868061f0fafd355
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-08 16:12:35 +0430

    Update the UserSelectionActivity to show users

--- app/src/main/kotlin/ir/mahozad/workout_logger/UserSelectionActivity.kt
+++ app/src/main/kotlin/ir/mahozad/workout_logger/UserSelectionActivity.kt
@@ -3,14 +3,22 @@ package ir.mahozad.workout_logger
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.AndroidEntryPoint
 import ir.mahozad.workout_logger.ui.theme.WorkoutLoggerTheme
 
+@AndroidEntryPoint
 class UserSelectionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +30,17 @@ class UserSelectionActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @Composable
-    fun UserSelectionScreen() {
-        Button(onClick = {}) {
-            Text(stringResource(R.string.start_workout))
+@Composable
+fun UserSelectionScreen(viewModel: UserSelectionViewModel = viewModel()) {
+    val users by viewModel.getAllUsers().collectAsState(emptyList())
+    Column(Modifier.testTag("users")) {
+        users.forEach { user ->
+            Text(user.firstName)
         }
+    }
+    Button(onClick = {}) {
+        Text(stringResource(R.string.start_workout))
     }
 }
