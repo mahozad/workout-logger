7caa960841809e0ff5933331054b484fa6b918da
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-09 19:21:30 +0430

    Rename MainActivityInstrumentedTest to MainActivityTest
    To match other test classes

--- app/src/androidTest/kotlin/ir/mahozad/workout_logger/MainActivityTest.kt
+++ app/src/androidTest/kotlin/ir/mahozad/workout_logger/MainActivityTest.kt
@@ -14,7 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityInstrumentedTest {
+class MainActivityTest {
 
     @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
