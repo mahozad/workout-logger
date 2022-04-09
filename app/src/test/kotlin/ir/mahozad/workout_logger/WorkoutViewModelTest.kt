bdb2b422b9b4e461a8a83c1827cd60db71fc0565
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-09 18:38:49 +0430

    Update WorkoutViewModel to use WorkoutRepository

---
+++ app/src/test/kotlin/ir/mahozad/workout_logger/WorkoutViewModelTest.kt
@@ -0,0 +1,22 @@
+package ir.mahozad.workout_logger
+
+import ir.mahozad.Workout
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.verify
+
+@ExtendWith(MockitoExtension::class)
+class WorkoutViewModelTest {
+
+    @Mock lateinit var workoutRepository: WorkoutRepository
+
+    @Test fun `Adding a workout should call the proper repository method`(): Unit = runBlocking {
+        val viewModel = WorkoutViewModel(workoutRepository)
+        val workout = Workout(0, 23, 19)
+        viewModel.addWorkout(workout.total, workout.correct)
+        verify(workoutRepository).addWorkout(workout)
+    }
+}
