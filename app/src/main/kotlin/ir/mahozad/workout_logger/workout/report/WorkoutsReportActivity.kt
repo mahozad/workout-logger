93a292320a08a128f1c7d683dcf3bf103b58fa1d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-04-12 19:20:58 +0430

    Add more information in the workout report screen

--- app/src/main/kotlin/ir/mahozad/workout_logger/workout/report/WorkoutsReportActivity.kt
+++ app/src/main/kotlin/ir/mahozad/workout_logger/workout/report/WorkoutsReportActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
@@ -15,8 +18,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
+import ir.mahozad.workout_logger.R
 import ir.mahozad.workout_logger.ui.theme.WorkoutLoggerTheme
 
 @AndroidEntryPoint
@@ -44,9 +50,22 @@ fun WorkoutsScreen(viewModel: WorkoutsReportViewModel = viewModel()) {
         val totalItems = workouts.values.sumOf { it.size }
         workouts
             .entries
-            .flatMap { it.value }
-            .forEachIndexed { index, workout ->
-                Text(workout.total.toString())
+            .flatMap { entry -> entry.value.map { entry.key to it } }
+            .forEachIndexed { index, item ->
+                val (user, workout) = item
+                Row {
+                    Text(user.firstName)
+                    Spacer(Modifier.width(8.dp))
+                    Text(user.lastName)
+                    Spacer(Modifier.width(12.dp))
+                    Text(stringResource(R.string.workout_total))
+                    Spacer(Modifier.width(4.dp))
+                    Text(workout.total.toString())
+                    Spacer(Modifier.width(12.dp))
+                    Text(stringResource(R.string.workout_correct))
+                    Spacer(Modifier.width(4.dp))
+                    Text(workout.correct.toString())
+                }
                 if (index < totalItems - 1) Divider(Modifier.testTag("divider"))
             }
     }
